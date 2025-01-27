2e12c66f1b5ce1ea7c7a663378776266ffcc86db
Josh Espinoza <joshespi@gmail.com>
2025-01-27 08:36:21 -0700

    bump version to 1.0.7 in PCSD Featured Area plugin

--- pcsd-featured-area.php
+++ pcsd-featured-area.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: PCSD Featured Area
   Description: Featured Area Controller. allows for single image, and videos in the featured area
-  Version: 1.0.6
+  Version: 1.0.7
   Author: Josh Espinoza
   Author URI: tech.provo.edu
 */
