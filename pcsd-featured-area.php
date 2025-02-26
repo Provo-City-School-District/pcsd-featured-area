09a7cc60834453e29c5cee14bf4f99842cbaaa20
Josh Espinoza <joshespi@gmail.com>
2025-02-26 07:23:14 -0700

    refactor featured area display logic to ensure global variables are available and improve readability

--- pcsd-featured-area.php
+++ pcsd-featured-area.php
@@ -20,35 +20,40 @@ function featured_area_display($content)
 {
   global $buildingImage; // Ensure the global $buildingImage variable is available
   global $post; // Ensure the global $post variable is available
-  $post_id = $post->ID; // Define the $post_id variable
 
-  // Check if we're inside the main loop in a single Post.
-  if (is_singular() && in_the_loop() && is_main_query()) {
+  if ($post) {
+    $post_id = $post->ID; // Define the $post_id variable
 
-    //Checks for Layout Variable
-    if (get_field('featured_layout_select')) {
-      if (get_field('featured_layout_select') == 'single') {
-        //Get Featured image value from Featured Area
+    // Check if we're inside the main loop in a single Post.
+    if (is_singular() && in_the_loop() && is_main_query()) {
+
+      //Checks for Layout Variable
+      if (get_field('featured_layout_select')) {
+        if (get_field('featured_layout_select') == 'single') {
+          //Get Featured image value from Featured Area
 ?>
-        <div class="featured-image-full" style="background-image: url(<?php echo get_field('featured_image'); ?>);" title="<?= get_field('featured_image_alt') ?>"></div>
-      <?php
-      } elseif (get_field('featured_layout_select') == 'video') {
-      ?>
-        <video controls poster="<?php echo get_field('video_poster'); ?>">
-          <source src="<?php echo get_field('video_file'); ?>" type="video/mp4">
-          Download the <a href="<?php echo get_field('video_file'); ?>">MP4</a> video.
-        </video>
-      <?php
-      } elseif (get_field('featured_layout_select') == 'none') {
-      }
-    } elseif (has_post_thumbnail())
-    //for post that existed before this plugin was developed checks for a featured image from the Wordpress core field and displays that image if the featured layout variable does not exist   
-    {
-      ?>
-      <div class="featured-image-full" style="background-image: url(<?php echo the_post_thumbnail_url(); ?>);"></div>
+          <div class="featured-image-full" style="background-image: url(<?php echo get_field('featured_image'); ?>);" title="<?= get_field('featured_image_alt') ?>"></div>
+        <?php
+        } elseif (get_field('featured_layout_select') == 'video') {
+        ?>
+          <video controls poster="<?php echo get_field('video_poster'); ?>">
+            <source src="<?php echo get_field('video_file'); ?>" type="video/mp4">
+            Download the <a href="<?php echo get_field('video_file'); ?>">MP4</a> video.
+          </video>
+        <?php
+        } elseif (get_field('featured_layout_select') == 'none') {
+        }
+      } elseif (has_post_thumbnail())
+      //for post that existed before this plugin was developed checks for a featured image from the Wordpress core field and displays that image if the featured layout variable does not exist   
+      {
+        ?>
+        <div class="featured-image-full" style="background-image: url(<?php echo the_post_thumbnail_url(); ?>);"></div>
     <?php
+      }
+      return $content;
+    } else {
+      return $content;
     }
-    return $content;
   } else {
     return $content;
   }
